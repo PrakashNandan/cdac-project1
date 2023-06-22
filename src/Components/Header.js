13c96e3213108eaf511d19ca664e84703e7d2ddd
Avviishek <avishekdastidar22@gmail.com>
2023-06-22 15:32:21 +0530

    final commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,7 +14,7 @@ function Header() {
   const onLogout = () => {
     dispatch(logout())
     dispatch(reset())
-    navigate('/');
+    navigate('/register');
     
     console.log("logout ho gya");
    
